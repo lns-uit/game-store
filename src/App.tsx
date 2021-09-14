ea04eff9c81bd392c5a60ecd7c66fedc9c043cb2
manhloi5501 <manhloi0505@gmail.com>
2021-09-14 21:07:16 +0700

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import './App.css';
 import { Spin, Space } from 'antd';
 import { useEffect, useState } from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
     }, 3000);
   }, []);
 
+  const toggleSideBar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className='app'>
-      <Spin size='large' spinning={isLoading} />
+      <p>This is text</p>
     </div>
   );
 }
---
+++ src/README.md
@@ -0,0 +1,14 @@
+# Rule
+
+- className css:
+
+  - Bem
+  - Example:
+    .block {} /_ Block _/
+    .block\_\_element {} /_ Element _/
+    .block--modifier {} /_ Modifier _/
+
+- Variable, function name:
+
+  - camelCase
+  - Example: loginUser, tokenUser,...
