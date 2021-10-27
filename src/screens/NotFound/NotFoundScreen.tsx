6a3e462b1c8275430e774f52fe52a6350a82ffcf
manhloi5501 <manhloi0505@gmail.com>
2021-10-27 10:13:23 +0700

    test commit

--- src/screens/NotFound/NotFoundScreen.tsx
+++ src/screens/NotFound/NotFoundScreen.tsx
@@ -2,9 +2,8 @@ import { Layout } from 'antd';
 import React from 'react';
 import './styles.css';
 
-
 function NotFoundScreen() {
-  return <div className="screen-not-found-container">not found</div>;
+  return <div className='screen-not-found-container'>not founded</div>;
 }
 
 export default NotFoundScreen;
