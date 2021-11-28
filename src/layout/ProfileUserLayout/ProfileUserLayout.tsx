86de770fd4a642535b92b1b594f78200fb210e7f
Your name here <khoitr190@gmail.com>
2021-11-28 22:25:08 +0700

    code background user and avatar

--- src/layout/ProfileUserLayout/ProfileUserLayout.tsx
+++ src/layout/ProfileUserLayout/ProfileUserLayout.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import "../../screens/User/styles.css"
 import { Avatar } from 'antd';
 function ProfileUserLayout(){
-    const backgroundUser = 'https://st.quantrimang.com/photos/image/2019/06/11/lol-la-gi-2.jpg'
+    const backgroundUser = 'https://st.quantrimang.com/photos/image/2019/06/11/lol-la-gi-2.jpg';
+    const user = {
+        realName: "Nguyen Phuc",
+        userName: "demo",
+        email: "demo@gmail.com",
+        phone: "0792545xxx"
+    }
     return (
         <div className="white">
             <div className="background-profile border-radius-8">
@@ -34,9 +40,17 @@ function ProfileUserLayout(){
             </div>
             <div className="information-profile">
                 <div className="flex-basic relative d-flex flex-shringk-1 min-width-0 column flex-grow-1 max-width-full">
-                    <div className="width-full pd-left-right-64 relative">
-                        <div>
-                            map dit
+                    <div className="width-full pd-left-right-48 relative">
+                        <div className="d-flex space-between">
+                            <div>
+                                <h1 className="m-bottom-4 white">{user.realName}</h1>
+                                <p className="m-bottom-4 gray-5">Username: {user.userName}</p>
+                                <p className="m-bottom-4 ">Email: {user.email}</p>
+                                <p className="m-0 ">Phone: {user.phone}</p>
+                            </div>
+                            <div>
+                                <div className="edit-profile">Edit Profile</div>
+                            </div>
                         </div>
                     </div>
                 </div>
