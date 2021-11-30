531d40be81a00607cc1528a2a8741da3b5c7fe46
Your name here <khoitr190@gmail.com>
2021-11-30 18:45:49 +0700

    done General

--- src/components/EditProfileGeneral/EditProfileGeneral.tsx
+++ src/components/EditProfileGeneral/EditProfileGeneral.tsx
@@ -1,8 +1,54 @@
 import React from 'react';
+import { Form, Input } from 'antd';
 
 function EditProfileGeneral(){
     return(
-        <div>map dit</div>
+        <div>
+            <div className="DialogHeader">About</div>
+            <div className="DialogBodyText">
+                Set your profile name and details. Providing additional information like your real name can help friends find you on the Steam Community.
+                <br/>
+                <br/>
+                Your profile name and avatar represent you throughout Steam, and must be appropriate for all audiences. Please see the for more details.
+            </div>
+            <div className="edit-general">
+                <div className="fs-16 lh-28 uppercase general-title">General</div>
+                <div className="pd-24-20">
+                    <div>
+                        <p className="DialogLabel">Username</p>
+                        <Form.Item
+                            name="username"
+                        >
+                            <Input placeholder="UserName"/>
+                        </Form.Item>
+                    </div>
+                    <div>
+                        <p className="DialogLabel">Real Name</p>
+                        <Form.Item
+                            name="realName"
+                        >
+                            <Input placeholder="Real Name"/>
+                        </Form.Item>
+                    </div>
+                    <div>
+                        <p className="DialogLabel">Email</p>
+                        <Form.Item
+                            name="email"
+                        >
+                            <Input placeholder="Email"/>
+                        </Form.Item>
+                    </div>
+                    <div>
+                        <p className="DialogLabel">Phone</p>
+                        <Form.Item
+                            name="phone"
+                        >
+                            <Input placeholder="Phone"/>
+                        </Form.Item>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
