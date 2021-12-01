5dd2da71f60cfa2e507f8b1d87411cad96fe0463
nguyenphuc1040 <nguyenphuc.it14@gmail.com>
2021-12-01 23:56:31 +0700

    complete comment game

--- src/components/Comment/CommentContainer.tsx
+++ src/components/Comment/CommentContainer.tsx
@@ -6,7 +6,7 @@ import {
 import { Avatar, Button, Input, Comment, Form, Rate } from "antd";
 import { CommentType, GameVersionType, UserType } from "../../interfaces/rootInterface";
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from "../../redux/reducers/index";
 import axios from "axios";
 import { Endpoint } from "../../api/endpoint";
@@ -15,18 +15,18 @@ import { existsTypeAnnotation } from "@babel/types";
 import numberDot from "../../utils/numberDot"
 const { TextArea } = Input;
 
-interface IdGame{
+interface IdGame {
     idGame: string;
 }
 
-function CommentContainer({idGame}:IdGame) {
+function CommentContainer({ idGame }: IdGame) {
     const [connection, setConnection] = useState<HubConnection>();
     const [comments, setComment] = useState<CommentType[]>([]);
     const [ms, setMs] = useState("");
     const [rateInput, setRateInput] = useState(-1);
-    const user = useSelector((state:RootState)=> state.user);
-    const { userName = '', idUser = ''} = user || {};
-    const [userInfo, setUserInfo]  = useState<UserType>();
+    const user = useSelector((state: RootState) => state.user);
+    const { userName = '', idUser = '' } = user || {};
+    const [userInfo, setUserInfo] = useState<UserType>();
     const [myComment, setMyComment] = useState<CommentType>();
     const [commentCount, setCommentCount] = useState(0);
 
@@ -41,21 +41,29 @@ function CommentContainer({idGame}:IdGame) {
                 console.log("message receive: ", message);
             });
 
-            connection.on("ReceiveCreateComment", (user , comment: CommentType) => {
+            connection.on("ReceiveCreateComment", (user, comment: CommentType) => {
                 console.log('comment is created')
-                if (comment.idUser === idUser)  setMyComment(comment);
-                setComment(cmts => [comment,...cmts])
-                setCommentCount(cmt => cmt+1)
+                if (comment.idUser === idUser) setMyComment(comment);
+                setComment(cmts => [comment, ...cmts])
+                setCommentCount(cmt => cmt + 1)
             })
 
             connection.on("ReceiveUpdateComment", (user, comment: CommentType) => {
-                setComment(cmts => [comment,...cmts.slice(cmts.findIndex(item => item.idComment === comment.idComment))])
+                if (comment.idUser === idUser) setMyComment(comment);
+
+                setComment(cmts => [
+                    ...cmts.slice(0, cmts.findIndex(item => item.idComment === comment.idComment)),
+                    comment,
+                    ...cmts.slice(cmts.findIndex(item => item.idComment === comment.idComment) + 1)
+                ])
+
+
             })
 
             connection.on("ReceiveDeleteComment", (user, id: string) => {
                 setMyComment(cmt => cmt?.idComment === id ? undefined : cmt);
                 setComment(cmts => cmts.filter(item => item.idComment !== id))
-                setCommentCount(cmt => cmt-1)
+                setCommentCount(cmt => cmt - 1)
             })
 
             await connection.start();
@@ -73,7 +81,7 @@ function CommentContainer({idGame}:IdGame) {
             console.log(e);
         }
     };
-    const createComment = async (data : CommentType) => {
+    const createComment = async (data: CommentType) => {
         try {
             await connection?.invoke("CreateComment", data)
         } catch (e) {
@@ -81,16 +89,16 @@ function CommentContainer({idGame}:IdGame) {
         }
     }
 
-    const updateComment = async (data : CommentType) =>{
+    const updateComment = async (data: CommentType, idUser: string, action: string) => {
         try {
-            await connection?.invoke("UpdateComment",data)
+            await connection?.invoke("UpdateComment", data, idUser, action)
         } catch (e) {
             console.log(e);
         }
     }
     const deleteComment = async (idcmt: string) => {
         try {
-            await connection?.invoke("DeleteComment",idcmt)
+            await connection?.invoke("DeleteComment", idcmt)
         } catch (e) {
             console.log(e);
         }
@@ -142,18 +150,18 @@ function CommentContainer({idGame}:IdGame) {
         </Form>
 
     );
-    
+
     const getComment = () => {
         return axios.get(Endpoint.mainApi + "api/comments/" + idGame).then((response) => {
-          setComment(response.data.reverse());
+            setComment(response.data.reverse());
         });
     };
 
     const getUserInfo = () => {
-        return axios.get( Endpoint.mainApi + "api/user/" + idUser,{
-          headers: {
-              Authorization: "Bearer "+ localStorage.getItem('accessToken') 
-          }
+        return axios.get(Endpoint.mainApi + "api/user/" + idUser, {
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem('accessToken')
+            }
         }).then((response) => {
             setUserInfo(response.data);
             joinRoom("stun-user", idGame);
@@ -161,23 +169,25 @@ function CommentContainer({idGame}:IdGame) {
             console.log(err)
         })
     };
-    const getCommentCount = () =>{
+    const getCommentCount = () => {
         return axios.get(Endpoint.mainApi + "api/comments/count/" + idGame)
-            .then (res => {
+            .then(res => {
                 setCommentCount(res.data)
             })
-            .catch (err=> console.log(err))
+            .catch(err => console.log(err))
     }
-    const getMyComment = () =>{
+    const getMyComment = () => {
         return axios.get(Endpoint.mainApi + "api/comments/" + idGame + "/" + idUser)
-                    .then(res => {{
-                        console.log(res)
-                        setMyComment(res.data);
-                    }})
-                    .catch (err => console.log(err))
+            .then(res => {
+                {
+                    console.log(res)
+                    setMyComment(res.data);
+                }
+            })
+            .catch(err => console.log(err))
     }
     useEffect(() => {
-        if (localStorage.getItem('accessToken')!==null) {
+        if (localStorage.getItem('accessToken') !== null) {
             getUserInfo();
         }
         getComment();
@@ -186,40 +196,43 @@ function CommentContainer({idGame}:IdGame) {
     }, []);
     return (
         <div>
-            <div className = "font-w500">
-                <div style ={{fontSize:"16px",display:"flex"}}>
+            <div className="font-w500">
+                <div style={{ fontSize: "16px", display: "flex" }}>
                     <div>
                         COMMENTS
                     </div>
                     &nbsp;&nbsp;&nbsp;&nbsp;
-                    <div style = {{color:"#6b6b6b"}}>
+                    <div style={{ color: "#6b6b6b" }}>
                         {numberDot(commentCount)} Comments
                     </div>
                 </div>
-                <br/> <br/>
-                <div className="rate-input-container">
-                {
-                    myComment?.idComment === undefined ?
-                    <Comment
-                        avatar={
-                            <Avatar src= {userInfo?.avatar===null ? "https://firebasestorage.googleapis.com/v0/b/docprintx.appspot.com/o/logoSecondary.png?alt=media&token=d451278d-c524-46b5-a400-816b7970baa8" : userInfo?.avatar}  alt="Han Solo" />
+                <br /> <br />
+                {idUser === '' ? null :
+                    <div className="rate-input-container">
+                        {
+                            myComment?.idComment === undefined ?
+                                <Comment
+                                    avatar={
+                                        <Avatar src={userInfo?.avatar === null ? "https://firebasestorage.googleapis.com/v0/b/docprintx.appspot.com/o/logoSecondary.png?alt=media&token=d451278d-c524-46b5-a400-816b7970baa8" : userInfo?.avatar} alt="Han Solo" />
+                                    }
+                                    content={<Editor />}
+                                /> : null
+
                         }
-                        content={<Editor />}
-                    /> : null
-                                        
+                    </div>
                 }
-                </div>
+
                 {
-                    myComment?.idComment !== undefined ? 
-                        <CommentItem comment = {myComment} updateCmtFunc = {updateComment} deleteCmtFunc = {deleteComment} isOwn = {true} />
-                    : null
+                    myComment?.idComment !== undefined ?
+                        <CommentItem idUser={idUser} comment={myComment} updateCmtFunc={updateComment} deleteCmtFunc={deleteComment} isOwn={true} />
+                        : null
                 }
-                <div style = {{borderBottom: "1px solid #6b6b6b",width: "100%"}}></div>
-                <br/> <br/>
+                <div style={{ borderBottom: "1px solid #6b6b6b", width: "100%" }}></div>
+                <br /> <br />
                 {comments.map((data, index) => (
                     data.idUser !== idUser ?
-                        <CommentItem comment = {data} updateCmtFunc = {updateComment} deleteCmtFunc = {deleteComment} isOwn = {idUser===data.idUser} />
-                    : null
+                        <CommentItem idUser={idUser} comment={data} updateCmtFunc={updateComment} deleteCmtFunc={deleteComment} isOwn={idUser === data.idUser} />
+                        : null
                 ))}
             </div>
         </div>
--- src/components/Comment/CommentItem.tsx
+++ src/components/Comment/CommentItem.tsx
@@ -12,29 +12,31 @@ interface CommentParam {
     updateCmtFunc: any;
     deleteCmtFunc: any;
     isOwn: boolean;
+    idUser: string;
 }
 
-interface LikeCommentType{
+interface LikeCommentType {
     idComment: any;
     idUser: any;
     isLike: boolean;
 }
 
-function Comment({comment,updateCmtFunc,deleteCmtFunc,isOwn}: CommentParam) {
-    const [likeCmts, setLikeCmts] = useState<LikeCommentType[]>([]);
-    const [disLikeCmts, setDislikeCmts] = useState<LikeCommentType[]>([]);
-    
-    const getLikeComment = () => {
-        return axios.get(Endpoint.mainApi + "api/likecomment/" + comment?.idComment)
-            .then (res => {
+function Comment({ comment, updateCmtFunc, deleteCmtFunc, isOwn, idUser }: CommentParam) {
+    const [userLike, setUserLike] = useState('null');
+
+    const checkMyLikeComment = () => {
+        console.log(idUser)
+        return axios.get(Endpoint.mainApi + "api/likecomment/" + comment?.idComment + "/" + idUser)
+            .then(res => {
                 console.log(res.data)
+                setUserLike(res.data.own)
             })
-            .catch (err => console.log(err))
+            .catch(err => console.log(err))
     }
-    
-    useEffect(()=>{
-        getLikeComment()
-    },[])
+
+    useEffect(() => {
+        checkMyLikeComment()
+    }, [comment])
     return (
         <div>
             <div key={comment?.idComment} className="comment-box">
@@ -43,7 +45,7 @@ function Comment({comment,updateCmtFunc,deleteCmtFunc,isOwn}: CommentParam) {
                         <Col xxl={8} xl={8} lg={8} md={8} sm={24} xs={24}>
                             <div className="d-flex gap-10 user">
                                 <div className="avatar">
-                                    <img src={comment?.avatar === null ? "https://firebasestorage.googleapis.com/v0/b/docprintx.appspot.com/o/logoSecondary.png?alt=media&token=d451278d-c524-46b5-a400-816b7970baa8": comment?.avatar} />
+                                    <img src={comment?.avatar === null ? "https://firebasestorage.googleapis.com/v0/b/docprintx.appspot.com/o/logoSecondary.png?alt=media&token=d451278d-c524-46b5-a400-816b7970baa8" : comment?.avatar} />
                                 </div>
                                 <div className="name">
                                     <span>{comment?.userName}</span>
@@ -55,15 +57,15 @@ function Comment({comment,updateCmtFunc,deleteCmtFunc,isOwn}: CommentParam) {
                                 <div>
                                     <Rate defaultValue={comment?.star} disabled />
                                     <p className="m-0 gray-4">
-                                        <Moment format = "MMMM DD YYYY, hh:mm:ss">
+                                        <Moment format="MMMM DD YYYY, hh:mm:ss">
                                             {comment?.time}
                                         </Moment>
-                                
+
                                     </p>
                                 </div>
                                 {isOwn ?
-                                    <div className = "btn-delete-cmt" onClick={()=>deleteCmtFunc(comment?.idComment)}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M14.12 10.47L12 12.59l-2.13-2.12-1.41 1.41L10.59 14l-2.12 2.12 1.41 1.41L12 15.41l2.12 2.12 1.41-1.41L13.41 14l2.12-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4zM6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9z"/></svg>
+                                    <div className="btn-delete-cmt" onClick={() => deleteCmtFunc(comment?.idComment)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M14.12 10.47L12 12.59l-2.13-2.12-1.41 1.41L10.59 14l-2.12 2.12 1.41 1.41L12 15.41l2.12 2.12 1.41-1.41L13.41 14l2.12-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4zM6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9z" /></svg>
                                     </div> : null
                                 }
                             </div>
@@ -75,40 +77,55 @@ function Comment({comment,updateCmtFunc,deleteCmtFunc,isOwn}: CommentParam) {
                                     Was this review helpful?
                                 </span>
                                 <div className="vote-btn d-flex gap-10">
-                                    <div className="d-flex flex-center-x" onClick = {()=>{
-                                        updateCmtFunc(comment);
+                                    <div className="d-flex flex-center-x"
+
+                                        onClick={() => {
+                                            if (idUser === "") alert("Login to do")
+                                            else updateCmtFunc(comment, idUser, "like");
                                         }}>
-                                        <div className="btnv6_grey_black">
+                                        <div className="btnv6_grey_black"
+                                            style={{
+                                                background: userLike === 'none' ? '#242424' : (userLike === '1' ? "#c23329" : "#242424"),
+                                                color: userLike === 'none' ? '#7c7c7c' : (userLike === '1' ? "white" : "#7c7c7c")
+                                            }}
+                                        >
                                             <span>
                                                 <i className="fa fa-thumbs-up"></i>
                                                 Yes
                                             </span>
                                         </div>
                                         &nbsp;
-                                        <div className = "comment-like-dislike-text">
+                                        <div className="comment-like-dislike-text">
                                             <p>{numberDot(comment?.likes)}</p>
                                         </div>
                                     </div>
                                     &nbsp;&nbsp;
                                     <div className="d-flex flex-center-x"
-                                          onClick = {()=>{
-                                            updateCmtFunc(comment);
-                                            }}
+
+                                        onClick={() => {
+                                            if (idUser === "") alert("Login to do")
+                                            else updateCmtFunc(comment, idUser, "dislike");
+                                        }}
                                     >
-                                        <div className="btnv6_grey_black">
+                                        <div className="btnv6_grey_black"
+                                            style={{
+                                                background: userLike === 'none' ? '#242424' : (userLike === '0' ? "#c23329" : "#242424"),
+                                                color: userLike === 'none' ? '#7c7c7c' : (userLike === '0' ? "white" : "#7c7c7c")
+                                            }}
+                                        >
                                             <span>
                                                 <i className="fa fa-thumbs-down"></i>
                                                 No
                                             </span>
                                         </div>
                                         &nbsp;
-                                        <div className = "comment-like-dislike-text">
+                                        <div className="comment-like-dislike-text">
                                             <p>{numberDot(comment?.dislike)}</p>
                                         </div>
                                     </div>
                                 </div>
-                               
-                                
+
+
                             </div>
                         </Col>
                     </Row>
