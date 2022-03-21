54d9a1e2e37ba9b60548c614a2a810a5fb7a984f
nguyenphuc1040 <nguyenphuc.it14@gmail.com>
2022-03-21 13:03:06 +0700

    add platform android

--- src/components/UploadFile/DetailGame.tsx
+++ src/components/UploadFile/DetailGame.tsx
@@ -141,7 +141,7 @@ function DetailGame(){
                     >
                         <Select placeholder="Please select platform">
                             <Option value="window">Window</Option>
-                            <Option value="macOS">MacOS</Option>
+                            <Option value="macOS">Android</Option>
                         </Select>
                     </Form.Item>
 
