8b5734f741d0e61fdd99b4d49392a86bdc8fa13f
nguyenphuc1040 <nguyenphuc.it14@gmail.com>
2022-06-12 23:24:54 +0700

    udpate

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -5,13 +5,13 @@ import 'firebase/compat/storage';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyATkUCik1Ow1YCH9UT8T_K8QyMIechZzVg",
-    authDomain: "web-game-326ed.firebaseapp.com",
-    projectId: "web-game-326ed",
-    storageBucket: "web-game-326ed.appspot.com",
-    messagingSenderId: "737770284154",
-    appId: "1:737770284154:web:7af84a4f9ca401d63dc7c3",
-    measurementId: "G-K7XLF0QQZ2"
+  apiKey: "AIzaSyBdZsclVdhNO5SQUxkq-5500gCfiOMushE",
+  authDomain: "cloudstunapp.firebaseapp.com",
+  projectId: "cloudstunapp",
+  storageBucket: "cloudstunapp.appspot.com",
+  messagingSenderId: "936873551926",
+  appId: "1:936873551926:web:ba7f4d89f5a1e329f01097",
+  measurementId: "G-MVRTZNLMQV"
 };
 
 firebase.initializeApp(firebaseConfig) 
